3f2757887f05e51953f82ad40a46036d42d550e6
Xander Forrest <xander@xanderforrest.com>
2020-05-08 01:25:16 +0100

    Added create blog post page, with test form that only accepts title of post.
    Refactored some of the HTML code to make the spacing under the header in the topmost template so it doesn't have to be repeated.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utilities import ContentManager
 app = Flask(__name__)
 
@@ -20,6 +20,14 @@ def blog():
 def post(blogid):
     post_data = CM.get_post(blogid)
     return render_template('post.html', post=post_data)
+
+
+@app.route('/blog/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'GET':
+        return render_template('blogbuilder.html')
+    elif request.method == 'POST':
+        return request.form["header"]
 
 
 if __name__ == '__main__':
